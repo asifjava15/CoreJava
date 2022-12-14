63ff098d9ad5bebce15ced5310c41430799b9d79
asifjava15 <asif_java@outlook.com>
2022-12-13 20:49:12 -0500

    Demo example commit

---
+++ src/com/training/core/GitPractice.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.training.core;
+
+/**
+ * @author Asif_
+ *
+ */
+public class GitPractice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Demo class for Git");
+	}
+
+}
